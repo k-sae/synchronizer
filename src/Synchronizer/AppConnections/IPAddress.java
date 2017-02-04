fcab514477858fe4d2732bf2bec4bd9f74696542
kareem <karim.salah2048@gmail.com>
2017-02-04 19:56:05 +0800

    add class for ip Address
    ip address class will make calculations on ip address little bit easier

---
+++ src/Synchronizer/AppConnections/IPAddress.java
@@ -0,0 +1,34 @@
+package Synchronizer.AppConnections;
+
+import java.util.Arrays;
+
+/**
+ * Created by kemo on 04/02/2017.
+ */
+public class IPAddress {
+   private byte[] ip;
+    public IPAddress(String s)
+    {
+        setIp(s);
+    }
+    public IPAddress(byte[] ip)
+    {
+        this.ip = ip;
+    }
+
+    public byte[] getIp() {
+        return ip;
+    }
+
+    public void setIp(byte[] ip) {
+        this.ip = ip;
+    }
+    public void setIp(String ipSTR) {
+        String[] ipNos = ipSTR.split(".");
+        ip = new byte[4];
+        if (ipNos.length > 4) throw new RuntimeException("invalid ip Address : " + Arrays.toString(ipNos));
+        for (int i = 0; i < 4; i++) {
+            ip[i] = Byte.parseByte(ipNos[i]);
+        }
+    }
+}
--- src/Synchronizer/AppConnections/Server/SeversScanner.java
+++ src/Synchronizer/AppConnections/Server/SeversScanner.java
@@ -1,12 +1,25 @@
 package Synchronizer.AppConnections.Server;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
  * Created by kemo on 04/02/2017.
  */
 public abstract class SeversScanner {
-    public SeversScanner() {
+    private int noOfThreads;
+    private int noOfWorkingThreads;
+    private String startingIp;
+    private String endingIp;
+    public SeversScanner(String startingIp) throws IOException {
+        noOfThreads = 4;
+        noOfWorkingThreads = 0;
+        this.startingIp = startingIp;
+    }
+
+    public void start()
+    {
+
     }
     public abstract void onFinish(ArrayList<ServerMetaData>  serversMetaData);
 }
