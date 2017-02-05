509472c8290a85f9bdc9b76dfc3cd70f027f74f3
kareem <karim.salah2048@gmail.com>
2017-02-05 19:51:16 +0800

    rename serversScanner to server finder modify how new instance is created remove some unnecessary req

--- src/Synchronizer/AppConnections/Server/ServersFinder.java
+++ src/Synchronizer/AppConnections/Server/ServersFinder.java
@@ -2,28 +2,27 @@ package Synchronizer.AppConnections.Server;
 
 import Connections.IPAddress;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 /**
  * Created by kemo on 04/02/2017.
  */
-public abstract class SeversScanner {
+public abstract class ServersFinder {
     private int noOfThreads;
     private int noOfWorkingThreads;
     private IPAddress startingIp;
     private IPAddress endingIp;
     private ArrayList<ServerMetaData> serversMetaData;
-    public SeversScanner(IPAddress startingIp) {
+    public ServersFinder() throws UnknownHostException {
         noOfThreads = 4;
         noOfWorkingThreads = 0;
-        this.startingIp = startingIp;
+        startingIp = new IPAddress( InetAddress.getLocalHost().getHostAddress());
+        startingIp.getIp()[3] = 1;
         serversMetaData = new ArrayList<>();
         endingIp = new IPAddress(startingIp.toString());
-        endingIp.getIp()[3] = (short) 254;
-    }
-    public SeversScanner(String startingIp)
-    {
-        this(new IPAddress(startingIp));
+        endingIp.getIp()[3] = 254;
     }
     public void start()
     {
