eb06fa6b196acac10cec3a236e7f1880ddc4e98f
kareem <karim.salah2048@gmail.com>
2017-02-05 21:07:37 +0800

    make the serverFinder work on five simultaneous threads

--- src/Synchronizer/AppConnections/Server/ServersFinder.java
+++ src/Synchronizer/AppConnections/Server/ServersFinder.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 /**
  * Created by kemo on 04/02/2017.
  */
+
+//this works on 5 simultaneous threads
 public abstract class ServersFinder {
     private int noOfThreads;
     private int noOfWorkingThreads;
@@ -26,7 +28,19 @@ public abstract class ServersFinder {
     }
     public void start()
     {
+        final short PART_SIZE = (short) ((endingIp.getIp()[3] - startingIp.getIp()[3]) / noOfThreads);
+        short portStart = startingIp.getIp()[3];
+        for (int i = 0; i < noOfThreads; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                short endIp = (short) (portStart + PART_SIZE);
+                if (finalI == noOfThreads - 1) endIp = (short) (endingIp.getIp()[3] + 1);
+                for (int j = portStart; j < endIp; j++) {
+                    //TODO
+                }
+            }).start();
 
+        }
     }
     public abstract void onFinish(ArrayList<ServerMetaData>  serversMetaData);
 }
