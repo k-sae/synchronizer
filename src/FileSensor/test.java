19e2ce9315a2b77a339cbbbbfa0d1a21aad291fe
Amr-Sameh <mrmero2006@gmail.com>
2017-02-07 02:57:01 +0200

    testing folder listners

---
+++ src/FileSensor/test.java
@@ -0,0 +1,57 @@
+package FileSensor;
+
+import com.sun.beans.util.Cache;
+import com.sun.xml.internal.ws.api.model.ParameterBinding;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
+
+public class test {
+
+    public static void main(String [] args){
+
+
+    
+    Path folder = Paths.get("F:\\a\\");
+
+
+        WatchService watchService = null;
+        try {
+            watchService = FileSystems.getDefault().newWatchService();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while (true){
+
+        try {
+            folder.register(watchService,
+                StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_MODIFY,
+                StandardWatchEventKinds.ENTRY_DELETE);
+            WatchKey watchKey;
+            do {
+                watchKey= watchService.take();
+                for( WatchEvent <?>event:watchKey.pollEvents()){
+
+                WatchEvent.Kind<?> kind= event.kind() ;
+                    Path f= (Path) event.context();
+                System.out.println(kind+"  :  "+f);}
+            }while (watchKey.reset());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            watchService.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}}
