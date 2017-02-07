1d3791414cd6c70d66ca858022164b78a8b62379
Amr-Sameh <mrmero2006@gmail.com>
2017-02-07 04:42:04 +0200

    added class to listen to folder need to analyze changes

---
+++ src/FileSensor/Folderlistener.java
@@ -0,0 +1,44 @@
+package FileSensor;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+/**
+ * Created by PC - MeiR on 2/7/2017.
+ */
+public class Folderlistener implements Runnable {
+    private Path FOLDER_PATH;
+    private WatchService watchService;
+    private WatchKey watchKey;
+
+
+
+    public Folderlistener(String path) throws IOException {
+        this.FOLDER_PATH = Paths.get(path);
+        this.watchService = FileSystems.getDefault().newWatchService();
+        this.FOLDER_PATH.register(watchService,
+                StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_MODIFY,
+                StandardWatchEventKinds.ENTRY_DELETE);
+    }
+
+
+    @Override
+    public void run() {
+
+        do {
+            try {
+                watchKey= watchService.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            for( WatchEvent <?>event:watchKey.pollEvents()){
+
+                WatchEvent.Kind<?> kind= event.kind() ;
+                Path f= (Path) event.context();
+                System.out.println(kind+"  :  "+f);}
+        }while (watchKey.reset());
+
+
+    }
+}
